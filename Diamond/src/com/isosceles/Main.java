59e3f45d2f3704d688a8375448eebe331d501a90
Evelyn Ribeiro <evelynrivi_@hotmail.com>
2018-04-30 16:40:52 -0300

    diamond isosceles completed

---
+++ Diamond/src/com/isosceles/Main.java
@@ -0,0 +1,26 @@
+package com.isosceles;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        var in = new Scanner(System.in);
+
+        System.out.print("Enter an integer number: ");
+        int n = in.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < (i * 2) + 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.print("\n");
+        }
+
+    }
+
+
+}
